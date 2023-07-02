dd58d64b94e97ff61e6dd36da03fea0055e5b2c3
Alexey Vakhov <vakhov@gmail.com>
2023-07-02 03:00:00 +0300

    add ruby 3.2.2 update goenv & cosmetic

--- build_images.sh
+++ build_images.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 export BUILD_VERSION="v1"
+export DEV_BASE_VERSION="v2"
 set -ex
 
 cat Dockerfile-unison | docker build -t tool2/unison:$BUILD_VERSION -
 docker push tool2/unison:$BUILD_VERSION
 
-cat Dockerfile-dev-base | docker build -t tool2/dev-base:$BUILD_VERSION -
-docker push tool2/dev-base:$BUILD_VERSION
+cat Dockerfile-dev-base | docker build -t tool2/dev-base:$DEV_BASE_VERSION -
+docker push tool2/dev-base:$DEV_BASE_VERSION
 
 (cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.6.6' -t tool2/dev-ruby:2.6.6-$BUILD_VERSION -
 docker push tool2/dev-ruby:2.6.6-$BUILD_VERSION
 
 (cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.7.5' -t tool2/dev-ruby:2.7.5-$BUILD_VERSION -
 docker push tool2/dev-ruby:2.7.5-$BUILD_VERSION
+
+(cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=3.2.2' -t tool2/dev-ruby:3.2.2-$BUILD_VERSION -
+docker push tool2/dev-ruby:3.2.2-$BUILD_VERSION
