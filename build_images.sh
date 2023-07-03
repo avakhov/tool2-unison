0f10db3773075cd8d70fabc9069e235a8a097fb7
Alexey Vakhov <vakhov@gmail.com>
2023-07-04 00:13:58 +0300

    2.7.4

--- build_images.sh
+++ build_images.sh
@@ -12,6 +12,9 @@ docker push tool2/dev-base:$DEV_BASE_VERSION
 (cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.6.6' -t tool2/dev-ruby:2.6.6-$BUILD_VERSION -
 docker push tool2/dev-ruby:2.6.6-$BUILD_VERSION
 
+(cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.7.4' -t tool2/dev-ruby:2.7.4-$BUILD_VERSION -
+docker push tool2/dev-ruby:2.7.4-$BUILD_VERSION
+
 (cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.7.5' -t tool2/dev-ruby:2.7.5-$BUILD_VERSION -
 docker push tool2/dev-ruby:2.7.5-$BUILD_VERSION
 
