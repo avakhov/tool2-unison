2ee487ae28fb6ebdb1b732314837948d87d6482c
Alexey Vakhov <vakhov@gmail.com>
2023-07-19 14:43:13 +0300

    add 2.5.1

--- build_images.sh
+++ build_images.sh
@@ -9,6 +9,9 @@ docker push tool2/unison:$BUILD_VERSION
 cat Dockerfile-dev-base | docker build -t tool2/dev-base:$DEV_BASE_VERSION -
 docker push tool2/dev-base:$DEV_BASE_VERSION
 
+(cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.5.1' -t tool2/dev-ruby:2.5.1-$BUILD_VERSION -
+docker push tool2/dev-ruby:2.5.1-$BUILD_VERSION
+
 (cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.6.6' -t tool2/dev-ruby:2.6.6-$BUILD_VERSION -
 docker push tool2/dev-ruby:2.6.6-$BUILD_VERSION
 
