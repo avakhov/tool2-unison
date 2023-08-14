e3203f0879c682054b8ceb2e3fcd296c126f94a5
Alexey Vakhov <vakhov@gmail.com>
2023-08-14 13:49:36 +0300

    update build_images

--- build_images.sh
+++ build_images.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# export DOCKER_BUILDX='docker buildx build --platform linux/amd64,linux/arm64 --push'
 if [ $(uname -m) == "x86_64" ]; then
   export ARCH="amd64"
 elif [ $(uname -m) == "arm64" ]; then
@@ -9,7 +8,7 @@ else
   echo "unknown architecture: $(uname -m)"
   exit 1
 fi
-export DOCKER_BUILDX="docker buildx build --platform linux/$ARCH --push"
+export DOCKER_BUILD="docker build"
 
 export UNISON_VERSION="v3"
 export DEV_BASE_VERSION="v4"
@@ -21,14 +20,14 @@ export RUBY_BUILD_VERSION_302="v3"
 export RUBY_BUILD_VERSION_322="v3"
 
 set -ex
-cat Dockerfile-unison | $DOCKER_BUILDX -t tool2/unison:$UNISON_VERSION-$ARCH -
-cat Dockerfile-dev-base | $DOCKER_BUILDX -t tool2/dev-base:$DEV_BASE_VERSION-$ARCH -
+cat Dockerfile-unison | docker build -t tool2/unison:$UNISON_VERSION-$ARCH -; docker push tool2/unison:$UNISON_VERSION-$ARCH
+cat Dockerfile-dev-base | docker build -t tool2/dev-base:$DEV_BASE_VERSION-$ARCH -; docker push tool2/dev-base:$DEV_BASE_VERSION-$ARCH
 
-(cat Dockerfile-dev-base; cat dev-ruby) | $DOCKER_BUILDX --build-arg='RUBY_VERSION=2.5.1' -t tool2/dev-ruby:2.5.1-$RUBY_BUILD_VERSION_251-$ARCH -
-(cat Dockerfile-dev-base; cat dev-ruby) | $DOCKER_BUILDX --build-arg='RUBY_VERSION=2.6.6' -t tool2/dev-ruby:2.6.6-$RUBY_BUILD_VERSION_266-$ARCH -
-(cat Dockerfile-dev-base; cat dev-ruby) | $DOCKER_BUILDX --build-arg='RUBY_VERSION=2.7.4' -t tool2/dev-ruby:2.7.4-$RUBY_BUILD_VERSION_274-$ARCH -
-(cat Dockerfile-dev-base; cat dev-ruby) | $DOCKER_BUILDX --build-arg='RUBY_VERSION=2.7.5' -t tool2/dev-ruby:2.7.5-$RUBY_BUILD_VERSION_275-$ARCH -
-(cat Dockerfile-dev-base; cat dev-ruby) | $DOCKER_BUILDX --build-arg='RUBY_VERSION=3.0.2' -t tool2/dev-ruby:3.0.2-$RUBY_BUILD_VERSION_302-$ARCH -
-(cat Dockerfile-dev-base; cat dev-ruby) | $DOCKER_BUILDX --build-arg='RUBY_VERSION=3.2.2' -t tool2/dev-ruby:3.2.2-$RUBY_BUILD_VERSION_322-$ARCH -
+(cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.5.1' -t tool2/dev-ruby:2.5.1-$RUBY_BUILD_VERSION_251-$ARCH -; docker push tool2/dev-ruby:2.5.1-$RUBY_BUILD_VERSION_251-$ARCH
+(cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.6.6' -t tool2/dev-ruby:2.6.6-$RUBY_BUILD_VERSION_266-$ARCH -; docker push tool2/dev-ruby:2.6.6-$RUBY_BUILD_VERSION_266-$ARCH
+(cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.7.4' -t tool2/dev-ruby:2.7.4-$RUBY_BUILD_VERSION_274-$ARCH -; docker push tool2/dev-ruby:2.7.4-$RUBY_BUILD_VERSION_274-$ARCH
+(cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=2.7.5' -t tool2/dev-ruby:2.7.5-$RUBY_BUILD_VERSION_275-$ARCH -; docker push tool2/dev-ruby:2.7.5-$RUBY_BUILD_VERSION_275-$ARCH
+(cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=3.0.2' -t tool2/dev-ruby:3.0.2-$RUBY_BUILD_VERSION_302-$ARCH -; docker push tool2/dev-ruby:3.0.2-$RUBY_BUILD_VERSION_302-$ARCH
+(cat Dockerfile-dev-base; cat dev-ruby) | docker build --build-arg='RUBY_VERSION=3.2.2' -t tool2/dev-ruby:3.2.2-$RUBY_BUILD_VERSION_322-$ARCH -; docker push tool2/dev-ruby:3.2.2-$RUBY_BUILD_VERSION_322-$ARCH
 
 echo done.
