7272a77ae5da6094b1e2e911a789a312b954026d
Alexey Vakhov <vakhov@gmail.com>
2024-09-18 17:06:45 +0300

    new images

--- build_images.rb
+++ build_images.rb
@@ -12,18 +12,17 @@ else
   exit 1
 end
 
-UNISON_VERSION = "v3"
-DEV_BASE_VERSION = "v6"
-RUBY_BUILD_VERSION_251 = "v3"
-RUBY_BUILD_VERSION_266 = "v3"
-RUBY_BUILD_VERSION_2610 = "v1"
-RUBY_BUILD_VERSION_273 = "v1"
-RUBY_BUILD_VERSION_274 = "v3"
-RUBY_BUILD_VERSION_275 = "v3"
-RUBY_BUILD_VERSION_302 = "v3"
-RUBY_BUILD_VERSION_311 = "v1"
-RUBY_BUILD_VERSION_322 = "v3"
-ELASTIC_VERSION_243 = "v4"
+ELASTIC_243 = "v6"
+DEV_BASE  = "v8"
+RUBY_251  = "v4"
+RUBY_266  = "v4"
+RUBY_2610 = "v2"
+RUBY_273  = "v2"
+RUBY_274  = "v4"
+RUBY_275  = "v4"
+RUBY_302  = "v4"
+RUBY_311  = "v2"
+RUBY_322  = "v4"
 
 def is_docker_image_exists?(image, tag)
   puts("cmd: curl -s https://hub.docker.com/v2/repositories/#{image}/tags/#{tag}")
@@ -31,39 +30,26 @@ def is_docker_image_exists?(image, tag)
   !!hr["id"]
 end
 
-def docker_build(image, tag, script, build_arg)
+def docker_build(image, tag, files)
   if is_docker_image_exists?(image, tag)
     puts "docker image #{image}:#{tag} already exists"
   else
-    puts "start #{image}:#{tag} build:"
-    system("#{script} | #{BUILD} #{build_arg ? "--build-arg=#{build_arg}" : ""} -t #{image}:#{tag} - && docker push #{image}:#{tag}")
+    puts "start #{image}:#{tag} build ..."
+    system("cd dockerfiles && (#{Array(files).map{|f| "cat #{f}"}.join("; ")}) | #{BUILD} -t #{image}:#{tag} - && docker push #{image}:#{tag}")
     raise "script failed" unless $?.success?
   end
 end
 
-def elastic_build(image, tag, folder)
-  if is_docker_image_exists?(image, tag)
-    puts "docker image #{image}:#{tag} already exists"
-  else
-    puts "start #{image}:#{tag} build:"
-    system("cd #{folder} && #{BUILD} -t #{image}:#{tag} . && docker push #{image}:#{tag}")
-    raise "script failed" unless $?.success?
-  end
-end
-
-docker_build("avakhov/unison", "#{UNISON_VERSION}-#{ARCH}", "cat Dockerfile-unison", nil)
-docker_build("avakhov/dev-base", "#{DEV_BASE_VERSION}-#{ARCH}", "cat Dockerfile-dev-base", nil)
-
-docker_build("avakhov/dev-ruby", "2.5.1-#{RUBY_BUILD_VERSION_251}-#{ARCH}", "(cat Dockerfile-dev-base; cat dev-ruby)", "RUBY_VERSION=2.5.1")
-docker_build("avakhov/dev-ruby", "2.6.6-#{RUBY_BUILD_VERSION_266}-#{ARCH}", "(cat Dockerfile-dev-base; cat dev-ruby)", "RUBY_VERSION=2.6.6")
-docker_build("avakhov/dev-ruby", "2.6.10-#{RUBY_BUILD_VERSION_2610}-#{ARCH}", "(cat Dockerfile-dev-base; cat dev-ruby)", "RUBY_VERSION=2.6.10")
-docker_build("avakhov/dev-ruby", "2.7.3-#{RUBY_BUILD_VERSION_273}-#{ARCH}", "(cat Dockerfile-dev-base; cat dev-ruby)", "RUBY_VERSION=2.7.3")
-docker_build("avakhov/dev-ruby", "2.7.4-#{RUBY_BUILD_VERSION_274}-#{ARCH}", "(cat Dockerfile-dev-base; cat dev-ruby)", "RUBY_VERSION=2.7.4")
-docker_build("avakhov/dev-ruby", "2.7.5-#{RUBY_BUILD_VERSION_275}-#{ARCH}", "(cat Dockerfile-dev-base; cat dev-ruby)", "RUBY_VERSION=2.7.5")
-docker_build("avakhov/dev-ruby", "3.0.2-#{RUBY_BUILD_VERSION_302}-#{ARCH}", "(cat Dockerfile-dev-base; cat dev-ruby)", "RUBY_VERSION=3.0.2")
-docker_build("avakhov/dev-ruby", "3.1.1-#{RUBY_BUILD_VERSION_311}-#{ARCH}", "(cat Dockerfile-dev-base; cat dev-ruby)", "RUBY_VERSION=3.1.1")
-docker_build("avakhov/dev-ruby", "3.2.2-#{RUBY_BUILD_VERSION_322}-#{ARCH}", "(cat Dockerfile-dev-base; cat dev-ruby)", "RUBY_VERSION=3.2.2")
-
-elastic_build("avakhov/elasticsearch", "2.4.3-#{ELASTIC_VERSION_243}-#{ARCH}", "elastic_243")
+docker_build("avakhov/elasticsearch", "2.4.3-#{ELASTIC_243}-#{ARCH}", "elastic_243")
+docker_build("avakhov/dev-base", "#{DEV_BASE}-#{ARCH}", "dev_base")
+docker_build("avakhov/dev-ruby", "2.5.1-#{RUBY_251}-#{ARCH}", ["dev_base", "ruby251"])
+docker_build("avakhov/dev-ruby", "2.6.6-#{RUBY_266}-#{ARCH}", ["dev_base", "ruby266"])
+docker_build("avakhov/dev-ruby", "2.6.10-#{RUBY_2610}-#{ARCH}", ["dev_base", "ruby2610"])
+docker_build("avakhov/dev-ruby", "2.7.3-#{RUBY_273}-#{ARCH}", ["dev_base", "ruby273"])
+docker_build("avakhov/dev-ruby", "2.7.4-#{RUBY_274}-#{ARCH}", ["dev_base", "ruby274"])
+docker_build("avakhov/dev-ruby", "2.7.5-#{RUBY_275}-#{ARCH}", ["dev_base", "ruby275"])
+docker_build("avakhov/dev-ruby", "3.0.2-#{RUBY_302}-#{ARCH}", ["dev_base", "ruby302"])
+docker_build("avakhov/dev-ruby", "3.1.1-#{RUBY_311}-#{ARCH}", ["dev_base", "ruby311"])
+docker_build("avakhov/dev-ruby", "3.2.2-#{RUBY_322}-#{ARCH}", ["dev_base", "ruby322"])
 
 puts "done :)"
